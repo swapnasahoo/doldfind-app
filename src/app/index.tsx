469b099001f362dab25715747cd130ac00f6cd21
SwapnaSahoo <sahooswapnaswarup@gmail.com>
2026-06-16 20:39:42 +0530

    Add NativeWind

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,17 +1,9 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View style={styles.container}>
-      <Text>Edit src/app/index.tsx to edit this screen.</Text>
+    <View className="flex-1 items-center justify-center">
+      <Text>DoldFind</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
