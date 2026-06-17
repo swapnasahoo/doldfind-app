aea2afd39c8acad326bd3200b74af41541bf3800
SwapnaSahoo <sahooswapnaswarup@gmail.com>
2026-06-17 12:34:45 +0530

    Add svg support
    Install react-native-svg and react-native-svg-transformer to enable SVG component support and configure Metro to handle SVG transformations.

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,15 @@
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativewind } = require("nativewind/metro");
- 
-/** @type {import('expo/metro-config').MetroConfig} */
+
 const config = getDefaultConfig(__dirname);
- 
+
+config.transformer.babelTransformerPath =
+  require.resolve("react-native-svg-transformer");
+
+config.resolver.assetExts = config.resolver.assetExts.filter(
+  (ext) => ext !== "svg",
+);
+
+config.resolver.sourceExts.push("svg");
+
 module.exports = withNativewind(config);
